c4f851587ffab1645364bcef3360e9914cb8c8db
WE634JG <gabriel.gamrat@gds.ey.com>
2021-03-21 17:07:37 +0100

    add sign out functionality and nav bar

--- Starter/IdentityNetCore/Controllers/IdentityController.cs
+++ Starter/IdentityNetCore/Controllers/IdentityController.cs
@@ -12,18 +12,21 @@ namespace IdentityNetCore.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IEmailSender _emailSender;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public IdentityController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
+        public IdentityController(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager, IEmailSender emailSender)
         {
             _emailSender = emailSender;
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
 
         public async Task<IActionResult> Signup()
         {
-            var model = new SignUpViewModel();
+            var model = new SignUpViewModel() { Role = "Member" };
             return View(model);
         }
 
@@ -33,6 +36,18 @@ namespace IdentityNetCore.Controllers
 
             if (ModelState.IsValid)
             {
+                if (!await _roleManager.RoleExistsAsync(model.Role))
+                {
+                    var role = new IdentityRole { Name = model.Role };
+                    var roleResult = await _roleManager.CreateAsync(role);
+                    if (!roleResult.Succeeded)
+                    {
+                        var errors = roleResult.Errors.Select(e => e.Description);
+                        ModelState.AddModelError("Role", string.Join("", errors));
+                        return View(model);
+                    }
+                }
+
                 if ((await _userManager.FindByEmailAsync(model.Email)) == null)
                 {
                     var user = new IdentityUser
@@ -45,6 +60,9 @@ namespace IdentityNetCore.Controllers
                     var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     if (result.Succeeded)
                     {
+                        //adds user to the role    
+                        await _userManager.AddToRoleAsync(user, model.Role);
+
                         var confirmationLink = Url.ActionLink("ConfirmEmail", "Identity", new { userId = user.Id, @token = token });
                         await _emailSender.SendEmailAsync("gabriel.gamrat@gmail.com", user.Email, "confirm you email", confirmationLink);
                         return RedirectToAction("Signin");
@@ -76,27 +94,41 @@ namespace IdentityNetCore.Controllers
         {
             if (ModelState.IsValid)
             {
+                var user = await _userManager.FindByEmailAsync(model.Username);
                 var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+                var isMember = await _userManager.IsInRoleAsync(user, "Member");
+                var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index");
+
+                    if (isMember)
+                    {
+                        return RedirectToAction("Member", "Home");
+                    }
+                    else if (isAdmin)
+                    {
+                        return RedirectToAction("Admin", "Home");
+                    }
                 }
                 else
                 {
-                    ModelState.AddModelError("Login", "Cannot login.");
-                    return View(model);
-                }
 
+                    ModelState.AddModelError("Login", "Cannot login.");
+                }
             }
-            else
-            {
-                return View(model);
-            }
+            return View(model);
         }
 
         public async Task<IActionResult> AccessDenied()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Signout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Signin");
         }
 
     }
--- Starter/IdentityNetCore/Models/SignUpViewModel.cs
+++ Starter/IdentityNetCore/Models/SignUpViewModel.cs
@@ -5,11 +5,13 @@ namespace IdentityNetCore.Models
     public class SignUpViewModel
     {
         [Required]
-        [DataType(DataType.EmailAddress, ErrorMessage ="Email address is missing or invalid.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email address is missing or invalid.")]
         public string Email { get; set; }
 
         [Required]
-        [DataType(DataType.Password, ErrorMessage ="Incorrect or missing password.")]
+        [DataType(DataType.Password, ErrorMessage = "Incorrect or missing password.")]
         public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
     }
 }
